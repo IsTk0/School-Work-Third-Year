25eb9ec78186587cacbd3ed4a45659599495b169
IsTk0 - Anto <97669353+IsTk0@users.noreply.github.com>
2022-03-19 10:21:42 +0100

    Rename Progetto numero pari o dispari.cpp to C++/Progetto numero pari o dispari.cpp

--- C++/Progetto numero pari o dispari.cpp
+++ C++/Progetto numero pari o dispari.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int main(){
 	int numero_inserito = 0; //inizializare la variabile di tipo intero
-	cout << "Questo programma capisce il numero da te inserito  pari o dispari. Prova tu a inserirne uno qui: ";
+	cout << "Questo programma capisce il numero da te inserito Å½ pari o dispari. Prova tu a inserirne uno qui: ";
 	cin >> numero_inserito; //inserire il numero da calcolare
     //il codice esegue il code inserito tra le paretesi solo se
-    //il resto  pari a 0 altrimente esegue il codice del else
+    //il resto Â pari a 0 altrimente esegue il codice del else
 	if (numero_inserito%2 == 0) {
-		cout << "Il numero da te inserito  pari";
+		cout << "Il numero da te inserito Å½ pari";
 	}
 	else {
-		cout << "Il numero da te inserito  dispari";
+		cout << "Il numero da te inserito Å½ dispari";
 	}
 }
