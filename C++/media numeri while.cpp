00d13e7ccad2b64387fcceb940e208ae9e70f7e6
IsTk0 - Anto <97669353+IsTk0@users.noreply.github.com>
2022-03-23 08:42:06 +0100

    Add files via upload

---
+++ C++/media numeri while.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+	int numero_numeri_totali = 0, numero_inserito = 0, somma_numeri_inseriti = 0, continua = 0, calcolo_media = 0;
+	
+	cout << "Benvenuto in questo programma. Inserisci i numeri di cui devi calcolare la media." << endl;
+	cout << "Vuoi inserire dei numeri? (0 no. 1 si): ";
+	cin >> continua;
+	
+	while(continua == 1){
+		numero_numeri_totali++;
+		cout << "Inserisci un numero-> ";
+		cin >> numero_inserito;
+		somma_numeri_inseriti = somma_numeri_inseriti + numero_inserito;
+		cout << "Vuoi continuare ad inserire dei numeri? (0 no. 1 si): ";
+		cin >> continua;
+		calcolo_media = somma_numeri_inseriti / numero_numeri_totali;
+		cout << "Attualmente la media e' pari a: " << calcolo_media << endl;
+		cout << "----------------------------------------------------------------------" << endl;
+	}
+	cout << "Arrivederci :)";
+}
